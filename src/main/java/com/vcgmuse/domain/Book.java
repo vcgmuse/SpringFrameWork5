4af11224e357da94c8b1f20daebf8c3f90918322
Emmanuel Vega <vcgmuse@gmail.com>
2021-02-09 21:14:51 -0600

    SpringFramework 5 Project

--- src/main/java/com/vcgmuse/domain/Book.java
+++ src/main/java/com/vcgmuse/domain/Book.java
@@ -18,7 +18,7 @@ public class Book {
   private String isbn;
 
   @ManyToMany
-  @JoinTable(name = "author_book", joinColumns = = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+  @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
   private Set<Author> authors;
 
   public Book(){}
@@ -51,5 +51,34 @@ public class Book {
 
   public void setAuthors(Set<Author> authors) {
     this.authors = authors;
+  }
+
+  @Override
+  public String toString() {
+    return "Book{" +
+        "id=" + id +
+        ", title='" + title + '\'' +
+        ", isbn='" + isbn + '\'' +
+        ", authors=" + authors +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Book book = (Book) o;
+
+    return id != null ? id.equals(book.id) : book.id == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return id != null ? id.hashCode() : 0;
   }
 }

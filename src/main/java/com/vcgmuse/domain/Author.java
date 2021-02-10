bc6d38429cdaad290f7632cb5e85f8ee0804b279
Emmanuel Vega <vcgmuse@gmail.com>
2021-02-09 21:06:27 -0600

    SpringFramework 5 Project

--- src/main/java/com/vcgmuse/domain/Author.java
+++ src/main/java/com/vcgmuse/domain/Author.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Author {
@@ -17,6 +18,8 @@ public class Author {
 
   private String firstName;
   private String lastName;
+
+  @ManyToMany(mappedBy = "authors")
   private Set<Book> books;
 
   public Author(){}
--- src/main/java/com/vcgmuse/domain/Book.java
+++ src/main/java/com/vcgmuse/domain/Book.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Book {
@@ -13,6 +16,9 @@ public class Book {
   private Long id;
   private String title;
   private String isbn;
+
+  @ManyToMany
+  @JoinTable(name = "author_book", joinColumns = = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
   private Set<Author> authors;
 
   public Book(){}

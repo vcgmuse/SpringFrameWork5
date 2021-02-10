121b6c7534e9b94c0ebc21b844a135a67b97f51a
Emmanuel Vega <vcgmuse@gmail.com>
2021-02-09 20:46:07 -0600

    Initial commit

---
+++ src/main/java/com/vcgmuse/Springframework5Application.java
@@ -0,0 +1,13 @@
+package com.vcgmuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springframework5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springframework5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcgmuse/Springframework5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcgmuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springframework5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

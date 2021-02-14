f0287415f8b345eb3eeca812f7d46603dc2ae266
Emmanuel Vega <vcgmuse@gmail.com>
2021-02-14 13:57:10 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/vcgmuse/repositories/AuthorRepository.java

--- src/main/java/com/vcgmuse/Springframework5Application.java
+++ src/main/java/com/vcgmuse/Springframework5Application.java
@@ -3,6 +3,7 @@ package com.vcgmuse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO: On number 20 Intro to Spring MVC
 @SpringBootApplication
 public class Springframework5Application {
 
